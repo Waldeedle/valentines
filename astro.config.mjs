3e7cd6a439f3e309f9c85391a51f9e87ce0036e7
waleed.masoom <smasoom@rippling.com>
2026-01-21 10:09:32 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://waleed.github.io',
+  site: 'https://waldeedle.github.io',
   base: '/valentines',
   output: 'static',
 });
